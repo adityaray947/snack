691d27874629e5898e28accc3b004e34f6769a32
aditya <adityaray947@gmail.com>
2024-08-12 15:40:05 +0530

    revert

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,15 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes.js');
 const cors = require('cors');
-const modelsRouter = require('./routes/modelsRouter.js');
-const path = require("path");
+const modelsRouter = require('./routes/modelsRouter.js')
+const path = require("path")
 
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json()); 
+
 app.use(cors()); 
 
 app.use('/api/user', userRoutes); 
@@ -19,16 +20,16 @@ app.use("/api", modelsRouter);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "../build")));
+  app.use(express.static(path.join(__dirname1,"/Frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "../build", "index.html"));
-  });
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "Frontend","build", "index.html"))
+  );
 } else {
   app.get("/", (req, res) => {
     res.send("API is running..");
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
