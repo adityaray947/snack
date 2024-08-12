3ecee05c10982a1509db866bc47928e5eb7d4c76
Aman83490 <101001395+Aman83490@users.noreply.github.com>
2024-08-12 15:13:15 +0530

    adding changes

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use("/api", modelsRouter);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/build")));
+  app.use(express.static(path.join(__dirname1,"/Frontend/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname1, "Frontend","build", "index.html"))
